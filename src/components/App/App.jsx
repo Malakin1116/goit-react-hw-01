79a62a8158f6ce162c80c8027bb46b10e9f7d4d7
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-07-31 12:47:25 +0300

    2 finished

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,24 @@
 
 import Profile from '../Profile/Profile'; 
-import css from './App.module.css';
+import FriendList from "../FriendList/FriendList/FriendList"
+
 import userData from '../userData/userData.json'; 
+import friendsData from "../userData/friends.json"
 
 export default function App() {
   return (
-    <div>
-      <Profile
-        className={css.profileStyle}
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-    </div>
+    <>
+    <Profile
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      image={userData.avatar}
+      stats={userData.stats}
+    />
+    <FriendList 
+   
+    friends={friendsData} />
+  </>
   );
 }
 
